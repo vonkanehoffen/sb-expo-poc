b3a3a5494334e5959ea56a27240faa1aa592ab88
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2024-10-24 19:31:05 +0100

    recommended config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 const config: StorybookConfig = {
   stories: [
@@ -12,10 +13,29 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-react-native-web",
+      options: {
+        modulesToTranspile: ["react-native-reanimated"],
+        babelPlugins: [
+          "@babel/plugin-proposal-export-namespace-from",
+          "react-native-reanimated/plugin",
+        ],
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
+  },
+  core: {
+    builder: "webpack5",
+  },
+  webpackFinal: async (config) => {
+    if (config.plugins) {
+      config.plugins.push(new NodePolyfillPlugin());
+    }
+    return config;
   },
 };
 export default config;
